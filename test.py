b398d6e44c7cd903172ea7e56df6a403402480cc
Ulrich Stroetz <ustroetz@gmail.com>
2013-06-11 16:05:47 -0700

    made main() func

--- test.py
+++ test.py
@@ -1,50 +1,53 @@
-import main_model as m
-from pprint import pprint
-import ogr, xlrd
+def main():
+    import main_model as m
+    from pprint import pprint
+    import ogr, xlrd
 
-### Harvest Type (clear cut = 0, partial cut = 1)
-PartialCut = 0
+    ### Harvest Type (clear cut = 0, partial cut = 1)
+    PartialCut = 1
 
-### GIS Data
-slope_raster = 'G:\\Basedata\\PNW\\terrain\\slope'
-elevation_raster = 'G:\\Basedata\\PNW\\terrain\\dem_prjr6'
-driver = ogr.GetDriverByName('ESRI Shapefile')
+    ### GIS Data
+    slope_raster = 'G:\\Basedata\\PNW\\terrain\\slope'
+    elevation_raster = 'G:\\Basedata\\PNW\\terrain\\dem_prjr6'
+    driver = ogr.GetDriverByName('ESRI Shapefile')
 
-property_shp = driver.Open('U:\\My Documents\Tool\\Data\\testarea5.shp', 0)
-property_lyr = property_shp.GetLayer()
-
-
-mill_shp = driver.Open('U:\\My Documents\Tool\\Data\\mills.shp', 0)
-mill_lyr = mill_shp.GetLayer()
-
-### Machine Costs
-harvest_mc_wb = xlrd.open_workbook('U:\\My Documents\Tool\\Data\\machinecost.xls')
-haul_mc_wb = xlrd.open_workbook('U:\\My Documents\Tool\\Data\\hauling.xls')
+    property_shp = driver.Open('U:\\My Documents\Tool\\Data\\testarea3.shp', 0)
+    property_lyr = property_shp.GetLayer()
 
 
-### Tree Data ###
+    mill_shp = driver.Open('U:\\My Documents\Tool\\Data\\mills.shp', 0)
+    mill_lyr = mill_shp.GetLayer()
 
-# Hardwood Fraction
-HdwdFractionCT = 0.15
-HdwdFractionSLT = 0.0
-HdwdFractionLLT = 0.0
+    ### Machine Costs
+    harvest_mc_wb = xlrd.open_workbook('U:\\My Documents\Tool\\Data\\machinecost.xls')
+    haul_mc_wb = xlrd.open_workbook('U:\\My Documents\Tool\\Data\\hauling.xls')
 
-# Chip Trees
-RemovalsCT = 0.0
-TreeVolCT = 0.0
 
-# Small Log Trees
-RemovalsSLT = 100.0
-TreeVolSLT = 50.0
+    ### Tree Data ###
 
-# Large Log Trees
-RemovalsLLT = 10.0
-TreeVolLLT = 80.0
+    # Hardwood Fraction
+    HdwdFractionCT = 0.15
+    HdwdFractionSLT = 0.0
+    HdwdFractionLLT = 0.0
 
-### Mill information
-millID = 335
-mill_Lat = None
-mill_Lon = None
+    # Chip Trees
+    RemovalsCT = 0.0
+    TreeVolCT = 0.0
 
-pprint (m.cost_func(haul_mc_wb, harvest_mc_wb, slope_raster, elevation_raster, property_lyr, mill_lyr, RemovalsCT, TreeVolCT, RemovalsSLT, TreeVolSLT, RemovalsLLT, TreeVolLLT, HdwdFractionCT, HdwdFractionSLT, HdwdFractionLLT, millID, mill_Lat, mill_Lon, PartialCut))
+    # Small Log Trees
+    RemovalsSLT = 100.0
+    TreeVolSLT = 50.0
 
+    # Large Log Trees
+    RemovalsLLT = 10.0
+    TreeVolLLT = 80.0
+    
+    ### Mill information
+    millID = None
+    mill_Lat = None
+    mill_Lon = None
+
+    pprint (m.cost_func(haul_mc_wb, harvest_mc_wb, slope_raster, elevation_raster, property_lyr, mill_lyr, RemovalsCT, TreeVolCT, RemovalsSLT, TreeVolSLT, RemovalsLLT, TreeVolLLT, HdwdFractionCT, HdwdFractionSLT, HdwdFractionLLT, millID, mill_Lat, mill_Lon, PartialCut))
+
+
+main()
